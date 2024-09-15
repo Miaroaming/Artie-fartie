a19eeab15e6fd3f1a838df3f3107cf59dff5c883
Miaroaming <mianzroaming@gmail.com>
2024-09-16 11:28:13 +1200

    first commit with sign up and login user controllers and models and routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+// import routes
+// const craftRoutes = require('./routes/crafts')
+const userRoutes = require('./routes/user')
+
+// Attach our Route to our app (express)
+// app.use('/api/crafts', craftRoutes)
+app.use('/api/user', userRoutes)
+
 // Home route for the backend
 app.get('/', (req, res) => {
     res.send('Hello, this is your Express server!'); 
