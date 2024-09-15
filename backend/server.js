c180b5b03e02828be12d9a0180918c1f58a6e766
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-09-16 11:23:29 +1200

    mongo db connection working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
+//require dotenv and envoke it
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const port = 4000;
+
+// mongo user name & password goes here  //
+const mongoUsername = process.env.MONGODB_USERNAME
+const mongoPassword = process.env.MONGODB_PASSWORD
+
+// mongo URI goes here //
+const mongoURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@crafts.gyzyb.mongodb.net/?retryWrites=true&w=majority&appName=crafts`;
 
 // Home route for the backend
 app.get('/', (req, res) => {
@@ -12,3 +23,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Express server is running on http://localhost:${port}`);
 });
+
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB Atlas:', err);
+    });
