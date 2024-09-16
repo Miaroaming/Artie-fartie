b4dafbdf3cd5850ae1a0c4764a1b0086c2a99b32
Miaroaming <mianzroaming@gmail.com>
2024-09-17 10:42:35 +1200

    step 15 to 19 finished

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+	return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'} )
+}
+
 // login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // signup user 
@@ -8,10 +25,11 @@ const signupUser = async (req,res) => {
     const {email, password} = req.body
 
     try {
-        // calls the custom signup static method defined in the User Model
         const user = await User.signup(email, password)
+
+        const token = createToken(user._id)
         
-        res.status(200).json({email, user})
+        res.status(200).json({email, token})
     } catch (error ) {
         res.status(400).json({error: error.message})
     }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { WorkoutsContextProvider } from './context/WorkoutContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+		<AuthContextProvider>
+	    <WorkoutsContextProvider>
+	      <App />
+	    </WorkoutsContextProvider>
+		</AuthContextProvider>
+  </React.StrictMode>,
+);
