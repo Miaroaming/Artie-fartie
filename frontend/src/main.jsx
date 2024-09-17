12c19ad9e0aa496310d8d02d60fab1cf38d19c7c
artcreatezart <abbie@Abbies-MBP.hub>
2024-09-17 15:55:42 +1200

    edited files Main.jsx to correct linking naming on lines 4, 10 and 12. I created craftForm Folder, CraftForm.jsx and craftForm.scss. I added all content into the craftform.jsx and craftform.scss. I did have the same previous error before with creat context not being created yet before a prior merge, but i know there may be a possible error in craftForm but due to it having a error prior i cant fix at the moment i wont be able to auctually see whether or not CraftForm all works

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { WorkoutsContextProvider } from './context/WorkoutContext.jsx'
+import { CraftsContextProvider } from './context/CraftContext.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 		<AuthContextProvider>
-	    <WorkoutsContextProvider>
+	    <CraftsContextProvider>
 	      <App />
-	    </WorkoutsContextProvider>
+	    </CraftsContextProvider>
 		</AuthContextProvider>
   </React.StrictMode>,
 );
