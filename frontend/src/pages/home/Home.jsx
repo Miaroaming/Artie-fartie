36185fea0c51ef7207774f2cff367cf2648648cf
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-09-17 12:10:54 +1200

    Finished craftcontext (step 9) could be some import/linking issues due to some parts not being done, so when we merge might just need to go over all of the issues in a seperate branch then push back to main so its all working before we all pull

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useCraftsContext } from '../../hooks/useCraftsContext'
+
+import CraftDetails from '../../components/nav/craftdetails/CraftDetails'
+import CraftForm from '../../components/nav/craftdetails/CraftForm'
 
 const Home = () => {
-  const [ crafts, setCrafts ] = useState( null )
+  const {crafts, dispatch} = useCraftsContext()
 
   useEffect(() => {
     const fetchCrafts = async () => {
 
+      const response = await axios.get('http://localhost:4000/api/crafts')
+
+      if (response.status === 200) {
+        console.log(response.data)
+        dispatch({type: 'SET_CRAFTS', payload: response.data})
+
+      }
 
     }
 
